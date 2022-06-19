89e09cd394e5d9478f3368428f1fcabaf5b40c0e
erinnliao@gmail.com <erinnliao@gmail.com>
2022-06-19 23:06:34 +0800

    feat: setting static files and add index.css fix: variable name - exphbs

--- app.js
+++ app.js
@@ -4,15 +4,18 @@ const app = express()
 const port = 3000
 
 // 載入handlebars
-const exhbs = require('express-handlebars')
+const exphbs = require('express-handlebars')
 
 //設定樣板引擎
-app.engine('handlebars', exhbs({ default: 'main'})) //(樣板引擎名稱, 預設佈局使用main的檔案)
+app.engine('handlebars', exphbs({ defaultLayout: 'main' })) //(樣板引擎名稱, 預設佈局使用main的檔案)
 app.set('view engine', 'handlebars') // 告訴Express要設定的view engine是handlebars
+
+// 告訴 Express 靜態檔案的資料夾位置
+app.use(express.static('public'))
 
 // 路由設定
 app.get('/', (req, res) => {
-  res.send('This is the Restaurant List built with Express')
+  res.render('index')
 })
 
 //監聽路由
