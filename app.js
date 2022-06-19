3bb8b383877699d77aae33c8992533271d221f24
erinnliao@gmail.com <erinnliao@gmail.com>
2022-06-19 22:12:29 +0800

    feat: setting template engine-handlebars、bootstrap

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// 載入handlebars
+const exhbs = require('express-handlebars')
+
+//設定樣板引擎
+app.engine('handlebars', exhbs({ default: 'main'})) //(樣板引擎名稱, 預設佈局使用main的檔案)
+app.set('view engine', 'handlebars') // 告訴Express要設定的view engine是handlebars
+
 // 路由設定
 app.get('/', (req, res) => {
   res.send('This is the Restaurant List built with Express')
